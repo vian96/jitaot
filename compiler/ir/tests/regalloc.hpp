8a27574d9f2892715416e7659567c70463802b1c
user.name <vianmart@yandex.ru>
2026-03-10 16:38:53 +0300

    add complex test

--- compiler/ir/tests/regalloc.hpp
+++ compiler/ir/tests/regalloc.hpp
@@ -125,6 +125,77 @@ static void verify_regalloc_semantics(const Graph &g, const char *test_name) {
     std::cout << "test was correct!\n";
 }
 
+static void build_multiple_phis_with_spill(Graph &g) {
+    BasicBlock *entry = &g.basic_blocks[0];
+    BasicBlock *left = &g.basic_blocks[1];
+    BasicBlock *right = &g.basic_blocks[2];
+    BasicBlock *join = &g.basic_blocks[3];
+
+    auto *cond =
+        entry->add_<EqBool>({entry->add_<Const64>({0}), entry->add_<Const64>({1})});
+    entry->add_next1(left);
+    entry->add_next2(right);
+
+    auto *l1 = left->add_<Const64>({11});  // phi1
+    auto *l2 = left->add_<Const64>({12});  // phi2
+    auto *l3 = left->add_<Const64>({13});  // phi3
+
+    // force l1, l2, l3 to stack
+    auto *lp1 = left->add_<Const64>({100});
+    auto *lp2 = left->add_<Const64>({101});
+    auto *lp3 = left->add_<Const64>({102});
+    auto *lp4 = left->add_<Const64>({103});
+    auto *l_sum1 = left->add_<Add64>({lp1, lp2});
+    auto *l_sum2 = left->add_<Add64>({l_sum1, lp3});
+    left->add_<Add64>({l_sum2, lp4});
+
+    left->add_next1(join);
+
+    auto *r1 = right->add_<Const64>({21});  // phi1
+    auto *r2 = right->add_<Const64>({22});  // phi2
+    auto *r3 = right->add_<Const64>({23});  // phi3
+
+    // force r1, r2, r3 to stack
+    auto *rp1 = right->add_<Const64>({200});
+    auto *rp2 = right->add_<Const64>({201});
+    auto *rp3 = right->add_<Const64>({202});
+    auto *rp4 = right->add_<Const64>({203});
+    auto *r_sum1 = right->add_<Add64>({rp1, rp2});
+    auto *r_sum2 = right->add_<Add64>({r_sum1, rp3});
+    right->add_<Add64>({r_sum2, rp4});
+
+    right->add_next1(join);
+
+    auto *phi1 = join->add_instruction(PHI_OPCODE, Types::INT64_T, {});
+    auto *phi2 = join->add_instruction(PHI_OPCODE, Types::INT64_T, {});
+    auto *phi3 = join->add_instruction(PHI_OPCODE, Types::INT64_T, {});
+
+    phi1->add_input(PhiInput{l1, left});
+    phi1->add_input(PhiInput{r1, right});
+
+    phi2->add_input(PhiInput{l2, left});
+    phi2->add_input(PhiInput{r2, right});
+
+    phi3->add_input(PhiInput{l3, left});
+    phi3->add_input(PhiInput{r3, right});
+
+    // force phi1, phi2, phi3 to stack
+    auto *jp1 = join->add_<Const64>({300});
+    auto *jp2 = join->add_<Const64>({301});
+    auto *jp3 = join->add_<Const64>({302});
+    auto *jp4 = join->add_<Const64>({303});
+    auto *j_sum1 = join->add_<Add64>({jp1, jp2});
+    auto *j_sum2 = join->add_<Add64>({j_sum1, jp3});
+    auto *j_sum3 = join->add_<Add64>({j_sum2, jp4});
+
+    // use phi
+    auto *p_sum1 = join->add_<Add64>({phi1, phi2});
+    auto *p_sum2 = join->add_<Add64>({p_sum1, phi3});
+    auto *final_res = join->add_<Add64>({p_sum2, j_sum3});
+
+    join->add_<RetVoid>({final_res});
+}
+
 inline void run_regalloc_unit_tests() {
     std::cout << "===  regalloc  ===\n\n";
 
@@ -159,6 +230,18 @@ inline void run_regalloc_unit_tests() {
         LinearScanRewriter re(&g, 2);
 
         verify_regalloc_semantics(g, "reducible loop");
+    }
+    {
+        Graph g(4);
+        build_multiple_phis_with_spill(g);
+        LoopAnalyzer la(&g);
+        LinearOrderBuilder lin(&g, &la);
+        LivenessAnalyzer live(lin, la);
+
+        LinearScanAllocator alloc(live, 3);
+        LinearScanRewriter re(&g, 3);
+
+        verify_regalloc_semantics(g, "Multiple parallel stack-to-stack PHIs (R=3)");
     }
 
     std::cout << "\nALL REGALLOC TESTS PASSED\n";
